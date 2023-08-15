290f9ebc144fb5bee5929fc5437739f36cbb903d
Murilothebr <murilothebr@hotmail.com>
2023-08-15 17:03:05 -0300

    feat: abstract class created

---
+++ base64/base64/Converter/ConvertFile.cs
@@ -0,0 +1,33 @@
+﻿using base64.Converter.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace base64.Converter;
+
+public class ConvertFile : ConvertAbstraction
+{
+    public override string DecodeFileFromBase64()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override string DecodeStringFromBase64()
+    {
+        throw new NotImplementedException();
+    }
+
+    #region error handler
+    public override string EncondeFileToBase64()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override string EncondeStringToBase64()
+    {
+        throw new NotImplementedException();
+    }
+    #endregion
+}
---
+++ base64/base64/Converter/Interface/ConvertAbstraction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace base64.Converter.Interface;
+
+public abstract class ConvertAbstraction
+{
+    public abstract string EncondeStringToBase64();
+
+    public abstract string DecodeStringFromBase64();
+
+    public abstract string EncondeFileToBase64();
+
+    public abstract string DecodeFileFromBase64();
+}
