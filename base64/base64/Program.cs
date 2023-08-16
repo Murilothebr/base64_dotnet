cf80e11725289f6b76059b3898ad194275024d93
Murilothebr <murilothebr@hotmail.com>
2023-08-15 23:01:43 -0300

    fix: cs proj files

--- base64/base64/Program.cs
+++ base64/base64/Program.cs
@@ -1,3 +1,16 @@
-﻿//ToDo:
+﻿using System;
+
+//Todo:
 //add flags
 //add chain handler / factory Software desing pattern
+
+namespace Base64Converter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"args: {args.Length}");
+        }
+    }
+}
