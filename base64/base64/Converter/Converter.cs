0c8fb087242115942dfdf298d664e443feac25f6
Murilothebr <murilothebr@hotmail.com>
2023-08-23 16:31:12 -0300

    renamed project

--- base64/base64/Converter/Converter.cs
+++ base64/base64/Converter/Converter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,21 +11,24 @@ public static class Converter
 {
     public static string EncodeToBase64String(string[] args)
     {
-        return $"teste string + {args[1]}";
+        return Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes(args[1]));
     }
 
     public static string DecodeFromBase64String(string[] args)
     {
-        return $"teste string + {args[1]}";
+        return System.Text.Encoding.UTF8.GetString(Convert.FromBase64String(args[1]));
     }
 
     public static string EncodeToBase64File(string[] args)
     {
-        return $"teste file + {args[1]}";
+        Byte[] bytes = File.ReadAllBytes(args[2]);
+        return Convert.ToBase64String(bytes);
     }
 
     public static string DecodeFromBase64File(string[] args)
     {
-        return $"teste file + {args[1]}";
+        Byte[] bytes = Convert.FromBase64String(args[2]);
+        File.WriteAllBytes(args[3], bytes);
+        return args[3];
     }
 }
