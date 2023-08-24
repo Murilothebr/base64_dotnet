540726a5df75c45253949bcdb4ff793dacc49cb1
Murilothebr <murilothebr@hotmail.com>
2023-08-24 10:10:04 -0300

    feat: todo

--- base64/base64/Program.cs
+++ base64/base64/Program.cs
@@ -3,26 +3,21 @@ using Base64Converter.Error_Handler;
 using Base64Converter.Handler;
 using System;
 
-//Todo:
-//add flags
-//add chain handler / factory Software desing pattern
+namespace Base64Converter;
 
-namespace Base64Converter
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            var stringHandler = new ConvertStringHandler();
-            var fileHandler = new ConvertFileHandler();
-            var helpHandlher = new HelpHandler();
+        var stringHandler = new ConvertStringHandler();
+        var fileHandler = new ConvertFileHandler();
+        var helpHandlher = new HelpHandler();
 
-            helpHandlher.SetNext(stringHandler);
-            stringHandler.SetNext(fileHandler);
+        helpHandlher.SetNext(stringHandler);
+        stringHandler.SetNext(fileHandler);
 
-            var handlerResult = Client.ClientCode(helpHandlher, args);
+        var handlerResult = Client.ClientCode(helpHandlher, args);
 
-            Console.WriteLine(handlerResult);
-        }
+        Console.WriteLine(handlerResult);
     }
 }
